f1a3532494fd437b002ae4427ce6969498931896
luiz lorenzo <luizf.lorenzo@gmail.com>
2026-05-14 20:00:55 -0300

    exercicio da aula5

--- main.dart
+++ main.dart
@@ -1,6 +1,28 @@
-void main () {
-  print('Hello World');
-  print('Welcome to Dart programming');
-  print ('This is a simple Dart program');
- print(3 + 4);
+class usuario {
+  int id;
+  String nome;
+  String? telefone;
+
+  usuario({required this.id, required this.nome, this.telefone});
+
+
 }
+
+
+void main() {
+ 
+  List<Map<String, dynamic>> usuarios = [
+    
+    {"id": 0000001, "nome":"Luiz", "fone": '4141-4141'},
+    {"id": 0000002, "nome":"Fernando", "fone": null},
+    {"id": 0000003, "nome":"Lorenzo", "fone": '5252-5252'},
+    {"id": 0000004, "nome":"Junior", "fone": '6363-6363'},
+  ];
+  List<String> nome = usuarios.map((user) {
+    return (user['nome'] as String).toUpperCase();
+  }).toList();
+
+  nome.forEach((elemento) {
+    print(elemento);
+  });
+ } 
